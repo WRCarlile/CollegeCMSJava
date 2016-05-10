8cf24065efb066d95dfd6ec0780099ecc11f3dbc
Nick Lyman and Ryan Carlile <nick@>
2016-05-10 16:48:21 -0700

    Add integration tests with front end application

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,140 +5,140 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/students", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("students", Student.all());
-    //   model.put("template", "templates/students.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/courses", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("courses", Course.all());
-    //   model.put("template", "templates/courses.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/courses", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   String course_title = request.queryParams("course_title");
-    //   Course newCourse = new Course(course_title);
-    //   newCourse.save();
-    //   response.redirect("/courses");
-    //   return null;
-    // });
-    //
-    // post("/students", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   String name = request.queryParams("name");
-    //   Student newStudent = new Student(name);
-    //   newStudent.save();
-    //   response.redirect("/students");
-    //   return null;
-    // });
-    //
-    // get("/courses/:id", (request,response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Course course = Course.find(Integer.parseInt(request.params("id")));
-    //   model.put("course", course);
-    //   model.put("allStudents", Student.all());
-    //   model.put("template", "templates/course.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/students/:id", (request,response) ->{
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Student student = Student.find(Integer.parseInt(request.params("id")));
-    //   model.put("student", student);
-    //   model.put("allCourses", Course.all());
-    //   model.put("template", "templates/student.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/add_courses", (request, response) -> {
-    //   int courseId = Integer.parseInt(request.queryParams("course_id"));
-    //   int studentId = Integer.parseInt(request.queryParams("student_id"));
-    //   Student student = Student.find(studentId);
-    //   Course course = Course.find(courseId);
-    //   student.addCourse(course);
-    //   response.redirect("/students/" + studentId);
-    //   return null;
-    // });
-    //
-    // post("/add_students", (request, response) -> {
-    //   int courseId = Integer.parseInt(request.queryParams("course_id"));
-    //   int studentId = Integer.parseInt(request.queryParams("student_id"));
-    //   Student student = Student.find(studentId);
-    //   Course course = Course.find(courseId);
-    //   course.addStudent(student);
-    //   response.redirect("/courses/" + courseId);
-    //   return null;
-    // });
-    // post("/courses/:id/edit", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Course course = Course.find(Integer.parseInt(request.params("id")));
-    //   String course_title = request.queryParams("course_title");
-    //   // Student student = Student.find(course.getStudentId());
-    //   course.update(course_title);
-    //   String url = String.format("/courses/%d", course.getId());
-    //   response.redirect(url);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/courses/:id/delete", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Course course = Course.find(Integer.parseInt(request.params("id")));
-    //   course.delete();
-    //
-    //   response.redirect("/courses");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/courses/:id/complete", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Course course = Course.find(Integer.parseInt(request.params("id")));
-    //   course.complete();
-    //   String url = String.format("/courses/%d", course.getId());
-    //   response.redirect(url);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/students/:id/edit", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Student student = Student.find(Integer.parseInt(request.params("id")));
-    //   String studentName = request.queryParams("studentName");
-    //   // Student student = Student.find(student.getStudentId());
-    //   student.update(studentName);
-    //   String url = String.format("/students/%d", student.getId());
-    //   response.redirect(url);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/students/:id/delete", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Student student = Student.find(Integer.parseInt(request.params("id")));
-    //   student.delete();
-    //
-    //   response.redirect("/students");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/students/:id/complete", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Student student = Student.find(Integer.parseInt(request.params("id")));
-    //   student.complete();
-    //   String url = String.format("/students/%d", student.getId());
-    //   response.redirect(url);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/students", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("students", Student.all());
+      model.put("template", "templates/students.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/courses", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("courses", Course.all());
+      model.put("template", "templates/courses.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/courses", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String course_title = request.queryParams("course_title");
+      String course_number = request.queryParams("course_number");
+
+      Course newCourse = new Course(course_title, course_number);
+      newCourse.save();
+      response.redirect("/courses");
+      return null;
+    });
+
+    post("/students", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String date = request.queryParams("date");
+      Student newStudent = new Student(name, date);
+      newStudent.save();
+      response.redirect("/students");
+      return null;
+    });
+
+    get("/courses/:id", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Course course = Course.find(Integer.parseInt(request.params("id")));
+      model.put("course", course);
+      model.put("allStudents", Student.all());
+      model.put("template", "templates/course.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/students/:id", (request,response) ->{
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Student student = Student.find(Integer.parseInt(request.params("id")));
+      model.put("student", student);
+      model.put("allCourses", Course.all());
+      model.put("template", "templates/student.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/add_courses", (request, response) -> {
+      int courseId = Integer.parseInt(request.queryParams("course_id"));
+      int studentId = Integer.parseInt(request.queryParams("student_id"));
+      int departmentId = Integer.parseInt(request.queryParams("department_id"));
+      Student student = Student.find(studentId);
+      Course course = Course.find(courseId);
+      Department department = Department.find(departmentId);
+      student.addCourse(course, department);
+      response.redirect("/students/" + studentId);
+      return null;
+    });
+
+    post("/add_students", (request, response) -> {
+      int courseId = Integer.parseInt(request.queryParams("course_id"));
+      int studentId = Integer.parseInt(request.queryParams("student_id"));
+      int departmentId = Integer.parseInt(request.queryParams("department_id"));
+      Student student = Student.find(studentId);
+      Course course = Course.find(courseId);
+      Department department = Department.find(departmentId);
+      course.addStudent(student, department);
+      response.redirect("/courses/" + courseId);
+      return null;
+    });
+    post("/courses/:id/edit", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Course course = Course.find(Integer.parseInt(request.params("id")));
+      String course_title = request.queryParams("course_title");
+      String course_number = request.queryParams("course_number");
+      // Student student = Student.find(course.getStudentId());
+      course.update(course_title, course_number);
+      String url = String.format("/courses/%d", course.getId());
+      response.redirect(url);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/courses/:id/delete", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Course course = Course.find(Integer.parseInt(request.params("id")));
+      course.delete();
+
+      response.redirect("/courses");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/courses/:id/complete", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Course course = Course.find(Integer.parseInt(request.params("id")));
+      course.complete();
+      String url = String.format("/courses/%d", course.getId());
+      response.redirect(url);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/students/:id/edit", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Student student = Student.find(Integer.parseInt(request.params("id")));
+      String studentName = request.queryParams("studentName");
+      String studentDate = request.queryParams("studentDate");
+      // Student student = Student.find(student.getStudentId());
+      student.update(studentName, studentDate);
+      String url = String.format("/students/%d", student.getId());
+      response.redirect(url);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/students/:id/delete", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Student student = Student.find(Integer.parseInt(request.params("id")));
+      student.delete();
+
+      response.redirect("/students");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,33 +1,33 @@
-// import org.sql2o.*;
-// import org.junit.*;
-// import org.fluentlenium.adapter.FluentTest;
-// import org.junit.ClassRule;
-// import org.junit.Test;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-// import static org.assertj.core.api.Assertions.assertThat;
-// import static org.fluentlenium.core.filter.FilterConstructor.*;
-// import static org.junit.Assert.*;
-//
-// public class AppTest extends FluentTest {
-//   public WebDriver webDriver = new HtmlUnitDriver();
-//
-//   @Override
-//   public WebDriver getDefaultDriver() {
-//     return webDriver;
-//   }
-//
-//   @ClassRule
-//   public static ServerRule server = new ServerRule();
-//
-//   @Rule
-//   public DatabaseRule database = new DatabaseRule();
-//
-//   @Test
-//   public void rootTest() {
-//     goTo("http://localhost:4567/");
-//     assertThat(pageSource()).contains("Todo list!");
-//   }
+import org.sql2o.*;
+import org.junit.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.junit.Assert.*;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Register here!");
+  }
 //
 //   @Test
 //   public void studentIsCreatedTest() {
@@ -93,4 +93,4 @@
 //     assertThat(pageSource()).contains("Household chores");
 //   }
 //
-// }
+}
